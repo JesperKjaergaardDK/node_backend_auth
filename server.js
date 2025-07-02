d4cae2c7a1168cbd635dda64bc4a7bd6fb4ac8fa
Jesper Kjærgaard <jezper@hotmail.dk>
2025-07-02 10:55:00 +0200

    feat: add JWT and session-based authentication with error handling page

--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const app = express();
+
 const path = require("path");
-const session = require("express-session");
 const bcrypt = require("bcrypt");
 const dotenv = require("dotenv");
+
+//Session
+const session = require("express-session");
 const MongoStore = require("connect-mongo");
+
+//JWT
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 
 dotenv.config();
 app.use(express.json());
@@ -17,16 +24,18 @@ const host = "http://localhost:" + port;
 // Start the server
 app.listen(port, () => console.log("Server starting... on " + host));
 
-let db;
 const { MongoClient } = require("mongodb");
 
 const client = new MongoClient(process.env.MONGO_URI);
 
+let db;
+
 async function connect() {
   try {
     const conn = await client.connect();
+    //Depending what auth to test
+    //db = await conn.db("jwt_test");
     db = await conn.db("session_test");
-    //db = await conn.db("session_test");
   } catch (error) {
     console.log(error);
   }
@@ -34,14 +43,15 @@ async function connect() {
 }
 connect();
 
-// Middleware when user mkas a request it will create a session if there are none or changes has happend to the exsisting one  
-app.use(
+//session based
+// Middleware when user mkas a request it will create a session if there are none or changes has happend to the exsisting one
+/* app.use(
   session({
     secret: process.env.SESSION_KEY,
     resave: false,
     saveUninitialized: false,
     cookie: {
-      maxAge: 1000 * 60 * 60, // 1 hour
+      maxAge: 1000 * 60, //1 min
       httpOnly: true,
       secure: false, // true in production with HTTPS
     },
@@ -52,24 +62,53 @@ app.use(
       ttl: 60 * 60, // 1 hour expiration
     }),
   })
-);
+); */
 
+/* function sessionAuth(req, res, next) {
+  if (req.session && req.session.userId) {
+    console.log(req.session);
+    console.log(req.session.userId);
+    
+    next(); // Session is valid
+  } else {
+    res.status(401).send("Unauthorized: No session");
+  }
+} */
 
-app.get("/", async (req, res) => {
-  const result = await connect();
+function JWTAuth(req, res, next) {
+  try {
+    const token = req.headers.authorization.split(" ")[1]; // "Bearer <token>"
+    if (!token) throw new error(500);
+
+    const decode = jwt.verify(token, process.env.JWT_SECRET);
+    console.log("this decode: " + decode);
+    if (!decode) throw new error(500);
+
+    next();
+  } catch (error) {
+    console.log(error);
+    res.sendFile(path.join(__dirname, "routes", "error.html"));
+  }
+}
+
+//Homepage
+app.get("/", sessionAuth, async (req, res) => {
+  const result = await db.collection("users").find().toArray();
   res.send(result);
 });
-
-
 
 //Register
 app.get("/register", async (req, res) => {
   res.sendFile(path.join(__dirname, "routes", "register.html"));
 });
 
-app.post("/register", async (req, res) => {
-  console.log("Received body:", req.body);
+//Login
+app.get("/login", async (req, res) => {
+  res.sendFile(path.join(__dirname, "routes", "login.html"));
+});
 
+//Register user for both auth
+app.post("/register", async (req, res) => {
   const { username, email, password, passwordConfirm } = req.body;
   if (!username || !email || !password || !passwordConfirm) {
     return res.status(400).send("Missing username or password");
@@ -94,12 +133,10 @@ app.post("/register", async (req, res) => {
 
 
 
-//Login
-app.get("/login", async (req, res) => {
-  res.sendFile(path.join(__dirname, "routes", "login.html"));
-});
 
-app.post("/login", async (req, res) => {
+
+//Session based authenticaiton
+/* app.post("/login", async (req, res) => {
   const { email, password } = req.body;
 
   if (!email || !password) {
@@ -122,6 +159,56 @@ app.post("/login", async (req, res) => {
     req.session.username = user.username;
 
     res.send(`Logged in as ${user.username}`);
+
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Login error");
+  }
+}); */
+
+
+
+
+//JWT based authenticaiton
+
+app.post("/login", async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).send("Missing email or password");
+  }
+
+  try {
+    const coll = db.collection("users");
+    const user = await coll.findOne({ email });
+    if (!user) return res.status(401).send("Invalid email or password");
+
+    //Signed jwt of token and refresh token
+    const token = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.JWT_SECRET,
+      { expiresIn: "1h" }
+    );
+
+    const refreshToken = jwt.sign(
+      { id: user._id, email: user.email },
+      process.env.RESET_SECRET,
+      { expiresIn: "7d" }
+    );
+
+    //send the refresh secure in the db
+    await db
+      .collection("tokens")
+      .insertOne({ userId: user._id, token: refreshToken });
+
+    //Sends the refresh token to a httponly cookie to protect agenst XSS attacks
+    res.cookie("refreshToken", refreshToken, {
+      httpOnly: true,
+      sameSite: "strict",
+      secure: false,
+    });
+
+    res.send(token);
 
   } catch (err) {
     console.error(err);
